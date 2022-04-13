ec8fe3cb8046b187a99042c33dcdd1af4fe34765
treemorse <dzondberg@gmail.com>
2022-04-13 19:27:22 +0300

    added files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pillar/screens/map_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
